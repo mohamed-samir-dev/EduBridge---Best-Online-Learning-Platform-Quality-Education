077d124fac5ef302f2ee55cf2b287bfdd3cd7caa
Mohammed Samier <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-08-11 22:18:27 +0300

    N: Main component that aggregates and renders the core sections of the page: // - Home: Homepage section // - Courses: Courses display section // - Reviews: User reviews section // - JoinUs: Call-to-action join section // All are wrapped inside a <main> element to define the primary page content

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "../Home/Home";
+import Courses from "../Courses/Courses";
+import Reviews from "../Reviews/Reviews";
+import JoinUs from "../JoinUs/JoinUs";
+
+export default function Main() {
+  return (
+    <main>
+      <Home />
+      <Courses />
+      <Reviews />
+      <JoinUs />
+    </main>
+  );
+}
